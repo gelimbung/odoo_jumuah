e7b45da1cdb5cd13baa0024e886176f58d7d2704
RIFKI <gel@RIFKIs-Air.bbrouter>
2024-06-24 22:05:56 +0700

    test

--- addons_custom/estate/models/estate_property_type_model.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-class EstatePropertyTypeModel(models.Model):
-    _name = "estate.property.type"
-    _description = "Estate Property Type"
-
-    name = fields.Char(string="Name", required=True)
